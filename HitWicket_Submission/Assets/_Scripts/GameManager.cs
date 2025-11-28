20a3ccc4b820492a6102f750e878d5c58cf6045f
Tanishq Srivastava <yashu_comp@hotmail.com>
2025-11-29 04:18:26 +0530

    LEVEL 2 DONE

--- HitWicket_Submission/Assets/_Scripts/GameManager.cs
+++ HitWicket_Submission/Assets/_Scripts/GameManager.cs
@@ -1,45 +1,45 @@
 using UnityEngine;
+using TMPro; // <--- 1. ADD THIS LIBRARY
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    public int score = 0;
     
-    // UI References
-    public GameObject startScreen;
-    public GameObject gameOverScreen;
-    public TMPro.TMP_Text scoreText;
-    void Awake() // <--- ADD THIS BLOCK
+    // 2. Add variables for Score
+    public int score = 0;
+    public TextMeshProUGUI scoreText; 
+
+    void Awake()
     {
         Instance = this;
     }
-    void Start() 
-    {
-         // Pause game at start
-         //Time.timeScale = 0;
 
-         //TEMPORARY REMOVE AFTER YOU GET UI
-         Time.timeScale = 1;
-    }
-
-    public void StartGame()
+    void Start()
     {
         
-        //startScreen.SetActive(false);
-        Time.timeScale = 1;
-        // Spawn the very first pulpit manually here
-        SpawnManager.Instance.SpawnPulpit(Vector3.zero); 
+        score = 0;
+        UpdateScoreUI();
+        Time.timeScale = 1; 
     }
 
+    
     public void AddScore()
     {
         score++;
-        scoreText.text = "Score: " + score;
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
     }
 
     public void GameOver()
     {
-       // Time.timeScale = 0;
-        //gameOverScreen.SetActive(true);
+        Time.timeScale = 0;
+       
     }
 }
--- HitWicket_Submission/Assets/_Scripts/Pulpit.cs
+++ HitWicket_Submission/Assets/_Scripts/Pulpit.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using TMPro; // For displaying the timer on the cube (optional but cool)
+using TMPro; 
 
 public class Pulpit : MonoBehaviour
 {
@@ -7,12 +7,13 @@ public class Pulpit : MonoBehaviour
     private float timer;
     private bool hasSpawnedNext = false;
 
-    // Reference to the text mesh to show countdown
+    
     public TextMeshPro timerText; 
+    private bool isScoreCounted = false;
 
     void Start()
     {
-        // Get random destroy time from JSON range
+        
         float minTime = ConfigManager.Instance.Config.pulpit_data.min_pulpit_destroy_time;
         float maxTime = ConfigManager.Instance.Config.pulpit_data.max_pulpit_destroy_time;
         
@@ -24,20 +25,29 @@ public class Pulpit : MonoBehaviour
     {
         timer -= Time.deltaTime;
 
-        // Update UI Text if you have it
+        
         if(timerText != null) timerText.text = timer.ToString("F1");
 
-        // Logic to Spawn the NEXT pulpit
+        
         if (timer <= ConfigManager.Instance.Config.pulpit_data.pulpit_spawn_time && !hasSpawnedNext)
         {
             hasSpawnedNext = true;
             SpawnManager.Instance.SpawnPulpit(this.transform.position);
         }
 
-        // Logic to Destroy THIS pulpit
+        
         if (timer <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        
+        if (other.gameObject.CompareTag("Player") && !isScoreCounted)
+        {
+            GameManager.Instance.AddScore();
+            isScoreCounted = true; 
         }
     }
 }
