bd853f56cf4331db0b583d6b1b13fa28e4d6e811
Tanishq Srivastava <yashu_comp@hotmail.com>
2025-11-29 03:40:13 +0530

    Made some basic beginner things, player can move now

---
+++ HitWicket_Submission/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int score = 0;
+    
+    // UI References
+    public GameObject startScreen;
+    public GameObject gameOverScreen;
+    public TMPro.TMP_Text scoreText;
+
+    void Start() 
+    {
+         // Pause game at start
+         Time.timeScale = 0;
+    }
+
+    public void StartGame()
+    {
+        startScreen.SetActive(false);
+        Time.timeScale = 1;
+        // Spawn the very first pulpit manually here
+        SpawnManager.Instance.SpawnPulpit(Vector3.zero); 
+    }
+
+    public void AddScore()
+    {
+        score++;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverScreen.SetActive(true);
+    }
+}
