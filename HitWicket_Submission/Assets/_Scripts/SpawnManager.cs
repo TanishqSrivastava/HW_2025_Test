06815fd1e6acc715b79d51de83112c50923512ca
Tanishq Srivastava <yashu_comp@hotmail.com>
2025-11-29 04:55:55 +0530

    LEVEL 2 DONE

--- HitWicket_Submission/Assets/_Scripts/SpawnManager.cs
+++ HitWicket_Submission/Assets/_Scripts/SpawnManager.cs
@@ -4,6 +4,7 @@ public class SpawnManager : MonoBehaviour
 {
     public static SpawnManager Instance;
     public GameObject pulpitPrefab;
+    private int previousDirection = -1;
 
     void Awake() { Instance = this; }
 
@@ -12,9 +13,16 @@ public class SpawnManager : MonoBehaviour
         Vector3 newPos = Vector3.zero;
         
         // Choose a random direction: 0=Forward, 1=Back, 2=Left, 3=Right
-        int direction = Random.Range(0, 4);
+        int direction = 0;
         float offset = 9f; // Since platform is 9x9
 
+        do 
+        {
+            direction = Random.Range(0, 4);
+        } 
+        while (IsOpposite(direction, previousDirection));
+
+        previousDirection = direction;
         switch (direction)
         {
             case 0: newPos = previousPosition + new Vector3(0, 0, offset); break; // North
@@ -22,12 +30,23 @@ public class SpawnManager : MonoBehaviour
             case 2: newPos = previousPosition + new Vector3(-offset, 0, 0); break; // West
             case 3: newPos = previousPosition + new Vector3(offset, 0, 0); break; // East
         }
-        if (newPos == Vector3.zero || newPos == previousPosition)
-        {
-            Debug.LogWarning("Spawn logic failed! Forcing Forward spawn.");
-            newPos = previousPosition + new Vector3(0, 0, offset);
-        }
+        
 
         Instantiate(pulpitPrefab, newPos, Quaternion.identity);
+    }
+    private bool IsOpposite(int newDir, int oldDir)
+    {
+        
+        if (oldDir == -1) return false;
+
+        
+        if (newDir == 0 && oldDir == 1) return true;
+        if (newDir == 1 && oldDir == 0) return true;
+
+        
+        if (newDir == 2 && oldDir == 3) return true;
+        if (newDir == 3 && oldDir == 2) return true;
+
+        return false;
     }
 }
